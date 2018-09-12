860719d55ca232346955e2a34d5b7c20c2f32df7
kirankn8 <kirankn8@gmail.com>
2018-09-12 22:21:52 +0530

    Add status codes

--- Backend/api/controllers/category.controller.js
+++ Backend/api/controllers/category.controller.js
@@ -1,30 +1,32 @@
 var categoryModel = require('../models/category.model');
+const httpStatusCode = require('../httpCodes');
 
 exports.new_category = function (req, res) {
     var categ = new categoryModel({ name: 'categ1' });
 
     categ.save(function (err, docs) {
-        if (err) return handleError(err);
-        res.json(docs);
+        if (err) res.status(500).json(httpStatusCode(500));
+        res.status(201).json(httpStatusCode(201));
     });
 }
 
 exports.find_category = function (req, res) {
     categoryModel.find({ _id: req.params.id }, function (err, docs) {
-        if (err) res.json(err);
+        if (err) res.status(404).json(httpStatusCode(404));
         res.json(docs);
     })
 }
 
 exports.update_category = function (req, res) {
     categoryModel.updateOne({ _id: req.params.id }, { name: req.body.name }, function (err, docs) {
-        if (err) return handleError(err);
+        if (err) res.status(500).json(httpStatusCode(500));
         res.json(docs);
     });
 }
 
 exports.delete_category = function (req, res) {
     categoryModel.deleteOne({ _id: req.params.id }, function (err) {
-        if (err) return handleError(err);
+        if (err) res.status(500).json(httpStatusCode(500));
+        res.json({ msg: "Object deleted successfully" });
     });
 }
---
+++ Backend/api/httpCodes.js
@@ -0,0 +1,64 @@
+var httpStatusObj = function (msg) {
+    return {
+        msg: msg
+    }
+}
+
+var httpStatusCode = function (status_code) {
+    switch (status_code) {
+        case 200: return httpStatusObj("OK")
+            break;
+        case 201: return httpStatusObj("Created")
+            break;
+        case 202: return httpStatusObj("Accepted")
+            break;
+        case 204: return httpStatusObj("No Content")
+            break;
+        case 301: return httpStatusObj("Permanently Moved")
+            break;
+        case 302: return httpStatusObj("Found")
+            break;
+        case 303: return httpStatusObj("See Other")
+            break;
+        case 304: return httpStatusObj("Not Modified")
+            break;
+        case 307: return httpStatusObj("Temporary Redirect")
+            break;
+        case 400: return httpStatusObj("Bad Request")
+            break;
+        case 401: return httpStatusObj("Unauthorized")
+            break;
+        case 403: return httpStatusObj("Forbidden")
+            break;
+        case 404: return httpStatusObj("Not Found")
+            break;
+        case 405: return httpStatusObj("Method Not Allowed")
+            break;
+        case 406: return httpStatusObj("Not Acceptable")
+            break;
+        case 412: return httpStatusObj("Precondition Failed")
+            break;
+        case 415: return httpStatusObj("Unsupported Media Type")
+            break;
+        case 500: return httpStatusObj("Internal Server Error")
+            break;
+        case 501: return httpStatusObj("Not Implemented")
+            break;
+        default:
+            switch (status_code / 100) {
+                case 1: return httpStatusObj("Communicates transfer protocol-level information.")
+                    break;
+                case 2: return httpStatusObj("Indicates that the client’s request was accepted successfully.")
+                    break;
+                case 3: return httpStatusObj("Indicates that the client must take some additional action in order to complete their request.")
+                    break;
+                case 4: return httpStatusObj("This category of error status codes points the finger at clients.")
+                    break;
+                case 5: return httpStatusObj("The server takes responsibility for these error status codes.")
+                    break;
+                default: return httpStatusObj("Unrecognized Status code")
+            }
+    }
+}
+
+module.exports = httpStatusCode;
